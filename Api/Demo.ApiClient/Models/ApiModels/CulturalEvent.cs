471aff49f3e5652e21fd99c7fb9832a10a17c26f
aj0325 <133852566+aj0325@users.noreply.github.com>
2024-05-08 12:24:04 +0530

    Add files via upload

---
+++ Api/Demo.ApiClient/Models/ApiModels/CulturalEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo.ApiClient.Models.ApiModels
+{
+    public class CulturalEvent
+    {
+        public int Id { get; set; }
+        public string? EventName { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public bool SignedUp { get; set; }
+    }
+}
---
+++ Api/Demo.ApiClient/Models/ApiModels/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.ApiClient.Models.ApiModels
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string? QuestionAsked { get; set; }
+        public DateTime Date { get; set; }
+        public string? Answer { get; set; }
+        public bool Solved { get; set; }
+
+    }
+}
