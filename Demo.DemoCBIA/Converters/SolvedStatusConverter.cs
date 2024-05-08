c7ec06296b10bd6609887b1ba00a4ca7a5cd2006
aj0325 <133852566+aj0325@users.noreply.github.com>
2024-05-08 12:48:00 +0530

    Add files via upload

---
+++ Demo.DemoCBIA/Converters/SolvedStatusConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DemoCBIA.Converters
+{
+    public class SolvedStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool solved)
+            {
+                return solved ? "Solved" : "Not Solved";
+            }
+
+            return "Not Solved";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
