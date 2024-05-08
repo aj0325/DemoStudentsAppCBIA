e5ea30b4c5d4063646fb7d3a9c4661d6cbd1e5e5
aj0325 <133852566+aj0325@users.noreply.github.com>
2024-05-08 12:46:55 +0530

    Add files via upload

---
+++ Demo.DemoCBIA/Models/Model.cs
@@ -0,0 +1,122 @@
+﻿using DevExpress.Maui.DataForm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DemoCBIA.Models
+{
+    public class AccommodationDetails
+    {
+        // Properties
+        public string RoomNumber { get; set; }
+        public string BuildingName { get; set; }
+        public string Floor { get; set; }
+        public bool IsSingleOccupancy { get; set; }
+        public int NumberOfRoommates { get; set; }
+        public List<string> RoommateNames { get; set; }
+
+        // Constructor
+        public AccommodationDetails()
+        {
+            RoommateNames = new List<string>();
+        }
+
+        // Method to add a roommate name to the list
+        
+    }
+
+    public class Question
+    {
+        public string StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string QuestionText { get; set; }
+        public string Status { get; set; } // "Pending" or "Solved"
+        public List<string> Answers { get; set; } = new List<string>();
+        // You can add properties for images or files as needed
+    }
+    public class Trip
+    {
+        public string Name { get; set; } 
+        public string Location { get; set; }
+        public bool GoingFormFilled { get; set; }
+        public string FullName { get; set; }
+        public string StudentId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactPhone { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string Purpose { get; set; }
+    }
+
+
+    public class CulturalEvent
+    {
+        public string EventName { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public bool SignedUp { get; set; }
+    }
+
+    public class Course
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string KeyDates { get; set; }
+        public string Events { get; set; }
+        public string Agreements { get; set; }
+    }
+
+    public class PdfModel
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class TripInformation
+    {
+        public string FullName { get; set; }
+        public string StudentId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string Purpose { get; set; }
+        // Add other properties as needed (e.g., transportation, lodging, medical info)
+    }
+    public class ProfileInfo
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public string DietaryPreference { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyPhoneNumber { get; set; }
+        public string EmergencyContactRelationship { get; set; }
+        public string ImageSource { get; set; }
+        public List<string> GenderOptions { get; } = new List<string> { "Female", "Male", "Other" };
+        public List<string> DietaryOptions { get; } = new List<string> { "Vegetarian", "Non-Vegetarian", "Vegan" };
+    }
+
+    
+
+    public class ProfileModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string DietaryPreference { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
---
+++ Demo.DemoCBIA/Models/QuestionStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DemoCBIA.Models
+{
+    public static class QuestionStorage
+    {
+        public static ObservableCollection<Question> Questions { get; set; } = new ObservableCollection<Question>();
+
+        public static void AddQuestion(string studentId, string studentName, string questionText)
+        {
+            Questions.Add(new Question
+            {
+                StudentId = studentId,
+                StudentName = studentName,
+                QuestionText = questionText,
+                Status = "Pending"
+            });
+        }
+    }
+}
